846c229180e1510d46bc5aff6470689a3ba5169e
SanushaSuresh <sanushasb17@gmail.com>
2025-07-13 19:10:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu=document.querySelector("#menu");
+const nav=document.querySelector("#link");
+menu.onclick=() => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
